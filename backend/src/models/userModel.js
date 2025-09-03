ac60fcc11e5b14d08146ea80740c6a8756c12b01
Md. Tanveer Ahmed <tanveera2001@gmail.com>
2025-09-03 23:11:12 +0600

    Created useModel.js

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,43 @@
+
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        
+        password: {
+            type: String,
+            required: true,
+        },
+
+        name: {
+            type: String,
+            required: true,
+        },
+
+        lastLogin: {
+            type: Date,
+            default: Date.now,
+        },
+
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+
+        resetPasswordToken: String,
+        resetPasswordExpiresAt: Date,
+        verificationToken: String,
+        verificationTokenExpiresAt: Date,
+    },
+    {timestamps: true}
+
+); 
+
+const User = model("User", userSchema);
+
+module.exports = User;
